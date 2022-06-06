be0c472eaf11a54fea0a61e45f7f936a0a444612
leslie-bund <lchukwunweike@gmail.com>
2022-06-06 04:57:15 +0100

    Fixed the file Tree bug

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -3,6 +3,11 @@ import FileTree from './fileTree';
 export function createFileTree(input) {
   const fileTree = new FileTree();
 
+  const rootDirNode = input.find(element => !element.parentId);
+  input.splice(input.indexOf(rootDirNode), 1);
+  input.sort((a, b) => a.id - b.id);
+  // input = [rootDirNode, ...input];
+  input.unshift(rootDirNode);
   for (const inputNode of input) {
     const parentNode = inputNode.parentId
       ? fileTree.findNodeById(inputNode.parentId)
